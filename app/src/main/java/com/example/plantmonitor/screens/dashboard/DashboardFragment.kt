30bf003c96f482f0e84f9802c8aab34e1ce27a4e
tmastrom <thomas.mastromonaco@gmail.com>
2020-06-11 16:45:09 -0700

    add ViewModel and databinding for soil humidity

--- app/src/main/java/com/example/plantmonitor/screens/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/plantmonitor/screens/dashboard/DashboardFragment.kt
@@ -6,10 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.plantmonitor.R
 import com.example.plantmonitor.databinding.DashboardFragmentBinding
+import com.example.plantmonitor.viewmodel.DashboardViewModel
 
 class DashboardFragment : Fragment() {
+
+    private lateinit var viewModel: DashboardViewModel
+    private lateinit var  binding : DashboardFragmentBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,8 +23,18 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val binding: DashboardFragmentBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
             inflater, R.layout.dashboard_fragment, container, false)
+        //val viewModelFactory =
+        viewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
+        updateHumidityValue()
         return binding.root
     }
+
+    /** Methods for updating the UI **/
+    private fun updateHumidityValue() {
+        binding.humidityValue.text = viewModel.humidity.toString()
+    }
+
+
 }
