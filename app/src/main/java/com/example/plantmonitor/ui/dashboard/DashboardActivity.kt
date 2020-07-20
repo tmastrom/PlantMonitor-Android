24b675e098b252fd252b920891852bdcc5cfbb45
Thomas Mastromonaco <thomas.mastromonaco@gmail.com>
2020-07-20 09:31:12 -0700

    remove toast msg from login activity

--- app/src/main/java/com/example/plantmonitor/ui/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/example/plantmonitor/ui/dashboard/DashboardActivity.kt
@@ -1,22 +1,13 @@
 package com.example.plantmonitor.ui.dashboard;
 
-import android.content.Context
 import android.os.Bundle
-import android.util.AttributeSet
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.plantmonitor.R
-import com.example.plantmonitor.data.models.PlantItem
 import com.example.plantmonitor.databinding.ActivityDashboardBinding
 import org.kodein.di.KodeinAware
-import org.kodein.di.KodeinContainer
 import org.kodein.di.android.kodein
 import org.kodein.di.generic.instance
 
